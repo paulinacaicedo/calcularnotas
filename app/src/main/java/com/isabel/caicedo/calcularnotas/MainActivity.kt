0546074a04705fd41d7782fa84d135c278797444
paulinacaicedo <149691365+paulinacaicedo@users.noreply.github.com>
2024-02-15 07:20:45 -0500

    Initial commit

---
+++ app/src/main/java/com/isabel/caicedo/calcularnotas/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.isabel.caicedo.calcularnotas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var CalcularNotas: TextView
+    private lateinit var retornar: Button
+    private lateinit var Nombre: TextView
+    private lateinit var editTextNumber4: EditText
+    private lateinit var editTextNumber7: EditText
+    private lateinit var enviar: Button
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        CalcularNotas = findViewById(R.id. )
+
+
+    }
+}
